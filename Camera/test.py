0f4666b58c3692ce624f9618fe6656c42ccaea42
Jorge <jorgefuert@gmail.com>
2025-03-16 11:03:21 +0100

    Camera test

---
+++ Camera/test.py
@@ -0,0 +1,16 @@
+# Script to check camera feed and Yolo inference
+
+import cv2 as cv
+
+camera = cv.VideoCapture(0)
+
+# Get image shape
+# f_height = int(camera.get(cv.CAP_PROP_FRAME_HEIGHT))
+# f_width = int(camera.get(cv.CAP_PROP_FRAME_WIDTH))
+
+while True:
+    cv.imshow("Camera", frame)
+    if cv.waitKey(1) == ord("q"):
+        break
+camera.release()
+cv.destroyAllWindows()
