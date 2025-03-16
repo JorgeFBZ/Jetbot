51f856de44870634f58568fbc42ec61e968be9af
Jorge <jorgefuert@gmail.com>
2025-03-16 15:53:29 +0100

    Camera test

--- Camera/test.py
+++ Camera/test.py
@@ -2,13 +2,16 @@
 
 import cv2 as cv
 
-# camera = cv.VideoCapture(0)
-camera = cv.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv.CAP_GSTREAMER)
+camera = cv.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv.CAP_GSTREAMER)
 
+if not camera.isOpened():
+    print("Error al conectar con la cámara.")
+
+cv.namedWindow("Camera", cv.WINDOW_AUTOSIZE)
 while True:
     ret, frame = camera.read()
     cv.imshow("Camera", frame)
     if cv.waitKey(1) == ord("q"):
         break
-camera.release()
 cv.destroyAllWindows()
+camera.release()
